bde48abcc0744b20add8d8a0145f6a27cbd884d5
sreya <sreyasunilkumar8@gmail.com>
2026-03-03 09:07:30 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -11,50 +11,65 @@ int main() {
     for (int i = 0; i < n; i++) {
         char pname[20];
         scanf("%s %d %d", pname, &at[i], &bt[i]);
-        pid[i] = atoi(pname + 1);
+        pid[i] = atoi(pname + 1);   // Extract number from P1, P2...
     }
 
-    // Check if input is already sorted by arrival time
-    int already_sorted = 1;
-    for (int i = 0; i < n - 1; i++)
-        if (at[i] > at[i + 1]) { already_sorted = 0; break; }
-
-    // Sort by arrival time
-    for (int i = 0; i < n - 1; i++)
-        for (int j = 0; j < n - i - 1; j++)
+    // Sort by arrival time (Bubble sort)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             if (at[j] > at[j + 1]) {
-                int t;
-                t=at[j];  at[j]=at[j+1];  at[j+1]=t;
-                t=bt[j];  bt[j]=bt[j+1];  bt[j+1]=t;
-                t=pid[j]; pid[j]=pid[j+1]; pid[j+1]=t;
-            }
+                int temp;
 
-    if (!already_sorted) {
-        // Use cumulative WT when sorting was needed
-        wt[0] = 0;
-        for (int i = 1; i < n; i++) wt[i] = wt[i-1] + bt[i-1];
-        for (int i = 0; i < n; i++) tat[i] = wt[i] + bt[i];
-    } else {
-        // Use proper FCFS when input was already in arrival order
-        int cur = 0;
-        for (int i = 0; i < n; i++) {
-            if (cur < at[i]) cur = at[i];
-            wt[i] = cur - at[i];
-            tat[i] = wt[i] + bt[i];
-            cur += bt[i];
+                temp = at[j];
+                at[j] = at[j + 1];
+                at[j + 1] = temp;
+
+                temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                temp = pid[j];
+                pid[j] = pid[j + 1];
+                pid[j + 1] = temp;
+            }
         }
     }
 
+    // Proper FCFS Calculation
+    int current_time = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        // If CPU is idle
+        if (current_time < at[i]) {
+            current_time = at[i];
+        }
+
+        wt[i] = current_time - at[i];
+        tat[i] = wt[i] + bt[i];
+
+        current_time += bt[i];
+    }
+
     double avgWT = 0, avgTAT = 0;
-    for (int i = 0; i < n; i++) { avgWT += wt[i]; avgTAT += tat[i]; }
+
+    for (int i = 0; i < n; i++) {
+        avgWT += wt[i];
+        avgTAT += tat[i];
+    }
+
     avgWT /= n;
     avgTAT /= n;
 
-    printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++) printf("P%d %d\n", pid[i], wt[i]);
-    printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++) printf("P%d %d\n", pid[i], tat[i]);
-    printf("Average Waiting Time: %.2f\n", avgWT);
+    printf("\nWaiting Time:\n");
+    for (int i = 0; i < n; i++)
+        printf("P%d %d\n", pid[i], wt[i]);
+
+    printf("\nTurnaround Time:\n");
+    for (int i = 0; i < n; i++)
+        printf("P%d %d\n", pid[i], tat[i]);
+
+    printf("\nAverage Waiting Time: %.2f\n", avgWT);
     printf("Average Turnaround Time: %.2f\n", avgTAT);
 
     return 0;
