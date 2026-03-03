7aeb9ba1ee593f48cec7dafda8b03c0bca4ed14e
sreya <sreyasunilkumar8@gmail.com>
2026-03-03 09:29:10 +0530

    Remove sorting by arrival time in FCFS.c
    Removed sorting logic for arrival times in FCFS scheduling.

--- FCFS.c
+++ FCFS.c
@@ -14,30 +14,10 @@ int main() {
         pid[i] = atoi(pname + 1);
     }
 
-    // Sort by arrival time
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (at[j] > at[j + 1]) {
-                int temp;
-
-                temp = at[j];
-                at[j] = at[j + 1];
-                at[j + 1] = temp;
-
-                temp = bt[j];
-                bt[j] = bt[j + 1];
-                bt[j + 1] = temp;
-
-                temp = pid[j];
-                pid[j] = pid[j + 1];
-                pid[j + 1] = temp;
-            }
-        }
-    }
-
     int current_time = 0;
 
     for (int i = 0; i < n; i++) {
+
         if (current_time < at[i])
             current_time = at[i];
 
